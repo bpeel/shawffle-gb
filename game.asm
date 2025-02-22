f4b71ef5434893be96714bbff6ec79ca6e6928d0
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-02-22 11:27:34 +0100

    Move UpdateKeys to utils

--- game.asm
+++ game.asm
@@ -737,43 +737,6 @@ SetTilePalettes:
         jr nc, .loop
         ret        
 
-        ;; Copied from https://gbdev.io/gb-asm-tutorial/part2/input.html
-UpdateKeys:
-        ; Poll half the controller
-        ld a, P1F_GET_BTN
-        call .onenibble
-        ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
-
-        ; Poll the other half
-        ld a, P1F_GET_DPAD
-        call .onenibble
-        swap a ; A3-0 = unpressed directions; A7-4 = 1
-        xor a, b ; A = pressed buttons + directions
-        ld b, a ; B = pressed buttons + directions
-
-        ; And release the controller
-        ld a, P1F_GET_NONE
-        ldh [rP1], a
-
-        ; Combine with previous wCurKeys to make wNewKeys
-        ld a, [CurKeys]
-        xor a, b ; A = keys that changed state
-        and a, b ; A = keys that changed to pressed
-        ld [NewKeys], a
-        ld a, b
-        ld [CurKeys], a
-        ret
-
-.onenibble:     
-        ldh [rP1], a ; switch the key matrix
-        call .knownret ; burn 10 cycles calling a known ret
-        ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
-        ldh a, [rP1]
-        ldh a, [rP1] ; this read counts
-        or a, $F0 ; A7-4 = 1; A3-0 = unpressed keys
-.knownret:      
-        ret
-
 HandleKeyPresses:
         ld a, [NewKeys]
         cp a, $10
@@ -1089,8 +1052,6 @@ SECTION "GameVariables", WRAM0
 UsedLetters:    db
 CorrectLetters: db
 SearchLetter:   db
-CurKeys:        db
-NewKeys:        db
 CursorX:        db
 CursorY:        db
 SelectionPos:   db              ; tile index or $ff if not set
--- globals.asm
+++ globals.asm
@@ -1,2 +1,4 @@
 SECTION "Globals", WRAM0
 VblankOccured:: db
+CurKeys::       db
+NewKeys::       db
